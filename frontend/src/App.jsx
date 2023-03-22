71eace892a94aab960f6ab0ab09c26c34f904689
huana <huy.huana@gmail.com>
2023-03-23 05:35:58 +0700

    change host to prod

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { NewsContextProvider } from "../contexts/NewsContext";
 import Navbar from "./components/Navbar";
 import NewsPage from "./components/News";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://feature-fast-news.d17zten96y7ebp.amplifyapp.com";
 
 const App = () => {
   const [newsItems, setNewsItems] = useState([]);
